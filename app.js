8c39fb4097d368e021b50e4d646d8dd7b1c3c9ce
lbmann <l.bmann@yahoo.com>
2021-01-04 20:31:09 -0500

    adding successful tests + successful inquire + writefilesync code

--- app.js
+++ app.js
@@ -36,3 +36,187 @@ const employees = [];
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
 
+(function buildManagerProfile() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Welcome to the Employee Database Program! What is your (the manager's) name?",
+            name: "name",
+            validate: (input) => {
+                if (input !== "" && typeof input == 'string') {
+                    return true;
+                } else {
+                    return "Please enter a valid name!"
+                }
+            }
+        },
+        {
+            type: "number",
+            message: "What is your employee id?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your email?",
+            name: "email",
+            validate: (input) => {
+                if (input !== "" && typeof input== 'string') {
+                    return true;
+                } else {
+                    return "Please enter a valid email to proceed!"
+                }
+            }
+        },
+        {
+            type: "input",
+            message: "What is your office number?",
+            name: "officeNumber"
+            
+        }
+        ]).then((response) => {
+            const name = response.name;
+            const id = response.id;
+            const email = response.email;
+            const officeNumber = response.officeNumber;
+    
+            const manager = new Manager(name, id, email, officeNumber);
+    
+            employees.push(manager);
+    
+            console.log(employees);
+    
+            addEmployees();
+           
+        })
+    
+    })();
+    
+    function addEmployees() {
+       inquirer.prompt([
+        {
+            type: "list",
+            message: "Add an employee, or are you finished with your team?",
+            name: "add",
+            choices: ["Add Employee", "Finish Team"]
+        }
+    
+        ]).then((response) => {
+            if (response.add == "Add Employee") {
+                console.log("adding a team member");
+    
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is the employee's name?",
+                        name: "name",
+                        validate: (input) => {
+                            if (input !== "" && typeof input == 'string') {
+                                return true;
+                            } else {
+                                return "Please enter a valid name!"
+                            }
+                        }
+                    },
+                    {
+                        type: "number",
+                        message: "What is the employee's id?",
+                        name: "id"
+                    },
+                    {
+                        type: "input",
+                        message: "What is the employee's email?",
+                        name: "email",
+                        validate: (input) => {
+                            if (input !== "" && typeof input== 'string') {
+                                return true;
+                            } else {
+                                return "Please enter a valid email to proceed!"
+                            }
+                        }
+                    },
+                    {   
+                        type: "list",
+                        message: "What kind of employee are you adding?",
+                        name: "employeeType",
+                        choices: ["Engineer", "Intern"]
+                    }
+                ]).then((response) => {
+    
+                    const name = response.name;
+                    const id = response.id;
+                    const email = response.email;
+    
+                    switch (response.employeeType) {
+                        case 'Engineer':
+                            makeEngineer(name, id, email);
+                            break;
+    
+                        case 'Intern':
+                            makeIntern(name, id, email);
+                            break;
+                    }
+                    
+                })
+    
+            }
+            else {
+    
+                try {
+                    const employeeHTML = render(employees);
+                    fs.writeFileSync(outputPath, employeeHTML);
+                    console.log("successfully wrote to file!")
+                }
+                catch(err) {
+                    console.log(err);
+                }
+    
+            }
+    
+        })};
+    
+    
+        function makeEngineer(name, id, email) {
+    
+            inquirer.prompt([
+                {   
+                    type: "input",
+                    message: "What is the engineer's Github username?",
+                    name: "github",
+                }
+            ]).then((response) => {
+    
+                const github = response.github;
+    
+                const engineer = new Engineer(name, id, email, github);
+    
+                employees.push(engineer);
+    
+                console.log(employees);
+    
+                addEmployees();
+    
+            })
+        };
+    
+        function makeIntern(name, id, email) {
+    
+            inquirer.prompt([
+                {   
+                    type: "input",
+                    message: "What is the intern's school?",
+                    name: "school",
+                }
+            ]).then((response) => {
+                
+                const school = response.school;
+    
+                const intern = new Intern(name, id, email, school);
+    
+                employees.push(intern);
+    
+                console.log(employees);
+    
+                addEmployees();
+    
+            })
+        };
