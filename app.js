a88e61c440e86046522f4a67a3107b7045a7d43e
lbmann <l.bmann@yahoo.com>
2021-01-04 20:46:21 -0500

    removing instructions and debug console logs from app.js and adding css file for html output

--- app.js
+++ app.js
@@ -12,30 +12,6 @@ const render = require("./lib/htmlRenderer");
 
 const employees = [];
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
 (function buildManagerProfile() {
     inquirer.prompt([
         {
@@ -83,8 +59,6 @@ const employees = [];
     
             employees.push(manager);
     
-            console.log(employees);
-    
             addEmployees();
            
         })
@@ -102,7 +76,6 @@ const employees = [];
     
         ]).then((response) => {
             if (response.add == "Add Employee") {
-                console.log("adding a team member");
     
                 inquirer.prompt([
                     {
@@ -164,7 +137,6 @@ const employees = [];
                 try {
                     const employeeHTML = render(employees);
                     fs.writeFileSync(outputPath, employeeHTML);
-                    console.log("successfully wrote to file!")
                 }
                 catch(err) {
                     console.log(err);
@@ -191,8 +163,6 @@ const employees = [];
     
                 employees.push(engineer);
     
-                console.log(employees);
-    
                 addEmployees();
     
             })
@@ -213,8 +183,6 @@ const employees = [];
                 const intern = new Intern(name, id, email, school);
     
                 employees.push(intern);
-    
-                console.log(employees);
     
                 addEmployees();
     
